a11778da1b6c23ceccc5e812b8fca549f941cd12
Danilo <daniloabella@gmail.com>
2016-10-18 10:54:05 -0300

    Trabajo terminado carajo!

--- a2.cpp
+++ a2.cpp
@@ -7,37 +7,34 @@ using namespace std;
 
 int main(int argc, char const *argv[]) 
 {
-    int i, b_int, a_int;
-  
-    mpz_t a, b, r;
+    mpz_t a, b, r, a_b;
 
+    mpz_init_set_ui (a_b,1);
     mpz_init_set_ui (r,1);
     mpz_init_set_str (a, argv[1], 10);
     mpz_init_set_str (b, argv[2], 10);
+    mpz_add ( a_b, a, b );
 
 // Decalración
-  
-    b_int = mpz_get_ui(b); 
-    a_int = mpz_get_ui(a); 
+ 
 
   if( argc != 3 ) { cout<<"Solo tiene que ingresar un valor para A y otro para B!"<<endl; }
-  else if( a_int < b_int ) { cout<<"Error! A tiene que ser >= que B"<<endl; }
-  else if( ( a_int + b_int ) <= 0 ) { cout<<"Error! B tiene que ser > 0 "<<endl; } 
+  else if( mpz_cmp ( a,b ) < 0 ) { cout<<"Error! A tiene que ser >= que B"<<endl; }
+  else if( mpz_cmp_ui ( a_b,0 ) <= 0 ) { cout<<"Error! (A+B) tiene que ser > 0 "<<endl; } 
   else if( ( mpz_cmp_ui ( a,0 ) < 0 ) || ( mpz_cmp_ui ( b,0 ) < 0 ) ) { cout<<"Error! A y B tienen que ser >= 0 "<<endl; }
   else{   
 
-  while( b_int != 0 )
+  while( mpz_cmp_ui ( b,0 ) != 0 )
   {
     mpz_init_set_ui (r,1); mpz_mod ( r, a, b );
 
-    mpz_init_set (a,b); mpz_init_set (b,r); b_int = mpz_get_ui(b); 
+    mpz_init_set (a,b); mpz_init_set (b,r);
 
   }
 
-  i = mpz_get_ui(a); 
+  cout<<"Valor de a:";	mpz_out_str(stdout,10,a);  cout<<"\n"<<endl;
 
-  cout<<"Valor de a:";  cout<<i<<endl;  // Lo puse solo para ver resultado final, pero se supone que devuelve "i".
-
+  mpz_clear( a_b );
   mpz_clear( a );
   mpz_clear( b );
   mpz_clear( r );
