86ad6a2ef4e2583b68f1a5477b8fc34a6dad63f0
Sergio Salinas <sergiosalfer@gmail.com>
2016-10-26 05:30:14 -0300

    agregado tiempo máximo

--- random.cpp
+++ random.cpp
@@ -5,7 +5,6 @@
 #include <gmp.h>                /* GNU GMP Library       */
 #include <math.h>
 #include <time.h>
-
 #include "a4.cpp"
 
 
@@ -17,40 +16,46 @@ int main(int argc, char const *argv[]) {
   int nBit; // 2^(nbit) cantidad de bits
   gmp_randstate_t state;
   gmp_randstate_t stateb;
+
+
   // a y b van a contener números random
   mpz_init(a);
   mpz_init(b);
   mpz_init(piso);
-  nBit = 12;
+  float MAX = 0;
+  nBit = 1200;
   mpz_ui_pow_ui(piso,2,nBit); // piso= 2^(nBit)
 
   gmp_randinit_default(state);
   gmp_randseed_ui(state, 10);
 
+  bool flag=0;
 	clock_t tStart;
   float t;
-  tStart = clock();
+
 
   for(int i = 0; i<=10; i++){
 
-    cout<<"a = ";
+    tStart = clock();
+
     mpz_urandomb(a, state, nBit);
     mpz_add(a,a,piso); // le suma a un número de 2^(nBit) para asegurarse que sea el minimo número a mostrar
-    mpz_out_str(stdout, 10, a);
-
-    cout<<"\t b = ";
     mpz_urandomb(b, state, nBit);
-    mpz_out_str(stdout, 10, b);
-    cout<<endl;
-    t = (double)(clock() - tStart)/CLOCKS_PER_SEC;
+
+    t += (double)(clock() - tStart)/CLOCKS_PER_SEC;
+
+
+    if(MAX < (double)(clock() - tStart)/CLOCKS_PER_SEC){
+      MAX=(double)(clock() - tStart)/CLOCKS_PER_SEC;
+    }
+    printf("\n i= %d t =  %.7f", i, (double)(clock() - tStart)/CLOCKS_PER_SEC);
   }
-  
-  printf("\nt =  %.7f \n", t);
+  printf("\nMAX =  %.7f \n", MAX);
+  printf("\nt =  %.7f \n", t/10);
   gmp_randclear(state);
   mpz_clear(a);
   mpz_clear(b);
   mpz_clear(piso);
-
   return 0;
 
 }
