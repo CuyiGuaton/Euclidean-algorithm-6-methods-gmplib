7cf1e18a800bff0346eee644c3650c2981ff408b
Sergio Salinas <sergiosalfer@gmail.com>
2016-10-26 19:44:04 -0300

    terminado

--- Raverageplot.R
+++ Raverageplot.R
@@ -9,7 +9,13 @@ for(i in 2:5){
   library(ggplot2) 
   plots <-ggplot(tabla, aes(x=names, y=mean)) + 
     geom_errorbar(aes(ymin=min, ymax=max), width=.5) +  geom_point() + ggtitle(paste("Algoritmo ",i,sep="")) +
-    labs(x="bits",y="Tiempo") 
+    labs(x="bits",y="Tiempo") +
+    theme_bw() +
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank()) 
   ggsave(plots,filename=paste("Informe/plot/a",i,".png",sep=""))
 
 }
